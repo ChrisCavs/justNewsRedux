3349d47e603cd59330372409e64759c8af4a38d6
Chris <chris211321@gmail.com>
2018-02-08 18:40:57 -0500

    style changes, title extension

--- src/js/renderArticle.js
+++ src/js/renderArticle.js
@@ -8,7 +8,7 @@ function renderArticle(article, key) {
     <a class='article-grid' href='${article.url}' target='_blank'>
       <div class='article-image'><img src=${article.urlToImage}></div>
       <div class='article-content'>
-        <p class='article-title'>${article.title.length > 70 ? article.title.slice(0,69) + '...' : article.title}</p>
+        <p class='article-title'>${article.title.length > 80 ? article.title.slice(0,79) + '...' : article.title}</p>
         <p class='article-body'>${article.description.length > 140 ? article.description.slice(0,139) + '...' : article.description}</p>
         <p class='article-time'>${article.publishedAt.slice(5,10) + ' at ' + article.publishedAt.slice(11,16) + ' - ' + article.source.name}</p>
       </div>
