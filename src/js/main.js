ded22e3b7f34f680557ebac82c0cd086da6e6312
Chris <chris211321@gmail.com>
2018-02-08 15:11:55 -0500

    formatting and buttons

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,22 @@ function setup() {
   //add event listeners on menu within modal
   if (fixed == 'no') {
 
+    document.querySelector('.edit-view').addEventListener('click', () => {
+      const currentViewContent = document.querySelector('.active');
+      const currentViewKey = document.querySelector('.active').classList[1];
+      revealModal(currentViewKey);
+    });
+
+    document.querySelector('.remove-view').addEventListener('click', () => {
+      let sourceObject = JSON.parse(window.localStorage.getItem('Data'));
+      const currentViewKey = document.querySelector('.active').classList[1];
+
+      delete sourceObject[currentViewKey];
+      let DATA = JSON.stringify(sourceObject);
+      window.localStorage.setItem('Data', DATA);
+      checkForData();
+    })
+
     Array.from(document.querySelectorAll('.accordian')).forEach(item => item.addEventListener('click', function () {
 
       //*****fix this so that it just toggles a class.  make sure that all the other panels are closed as well
