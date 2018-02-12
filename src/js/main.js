8eb3511e0767936f10d96a477ca08ede38b66947
Chris <chris211321@gmail.com>
2018-02-12 16:53:19 -0500

    refined main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,44 @@
 import {checkForData} from './checkForData';
 import {revealModal} from './revealModal';
 
-let fixed = 'no';
-
 function setup() {
 
   //check localStorage for data.  if there is data, generate the custom views
   checkForData();
 
   //add event listeners on menu within modal
-  if (fixed == 'no') {
+  document.querySelector('.edit-view').addEventListener('click', () => {
 
-    document.querySelector('.edit-view').addEventListener('click', () => {
+    const currentViewContent = document.querySelector('.active');
+    const currentViewKey = document.querySelector('.active').classList[1];
 
-      const currentViewContent = document.querySelector('.active');
-      const currentViewKey = document.querySelector('.active').classList[1];
+    revealModal(currentViewKey);
+  });
 
-      revealModal(currentViewKey);
-    });
+  document.querySelector('.remove-view').addEventListener('click', () => {
 
-    document.querySelector('.remove-view').addEventListener('click', () => {
+    let sourceObject = JSON.parse(window.localStorage.getItem('Data'));
+    const currentViewKey = document.querySelector('.active').classList[1];
 
-      let sourceObject = JSON.parse(window.localStorage.getItem('Data'));
-      const currentViewKey = document.querySelector('.active').classList[1];
+    delete sourceObject[currentViewKey];
+    let DATA = JSON.stringify(sourceObject);
+    window.localStorage.setItem('Data', DATA);
 
-      delete sourceObject[currentViewKey];
-      let DATA = JSON.stringify(sourceObject);
-      window.localStorage.setItem('Data', DATA);
+    checkForData();
+  })
 
-      checkForData();
-    })
+  //add listeners on accordians
+  Array.from(document.querySelectorAll('.accordian')).forEach(item => item.addEventListener('click', function () {
 
-    //add listeners on accordians
-    Array.from(document.querySelectorAll('.accordian')).forEach(item => item.addEventListener('click', function () {
+    const thisPanel = this.nextElementSibling;
+    const allPanels = Array.from(document.querySelectorAll('.panel'));
 
-      const thisPanel = this.nextElementSibling;
-      const allPanels = Array.from(document.querySelectorAll('.panel'));
+    //remove panel-active class from all panels
+    allPanels.forEach(panel => panel.classList.contains('panel-active') ? panel.classList.remove('panel-active') : null);
 
-      //remove panel-active class from all panels
-      allPanels.forEach(panel => panel.classList.contains('panel-active') ? panel.classList.remove('panel-active') : null);
-
-      //add panel-active class on click
-      thisPanel.classList.add('panel-active');
-    }));
-
-    fixed = 'yes';
-  }
+    //add panel-active class on click
+    thisPanel.classList.add('panel-active');
+  }));
 }
 
 document.addEventListener('DOMContentLoaded', setup);
